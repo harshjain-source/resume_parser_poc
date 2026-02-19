ea61fcf80949225c386b5422589fbbf8208bb7cb
Harsh <harsh.jain@vocso.com>
2026-02-19 18:17:06 +0530

    chagnes

--- tests/list_headers.py
+++ tests/list_headers.py
@@ -1,5 +1,5 @@
 import json
-
+# jhajdhfas
 sample_path = r"c:\Users\Dell\Desktop\resume_parser\resume_parser1\processed_resumes\BE.pdf_20260219_123106\clean_text.json"
 with open(sample_path, 'r', encoding='utf-8') as f:
     clean_text = json.load(f)["content"]
